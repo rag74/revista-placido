05fd6c4d98830b9956cc754ddac01dd1b43befe1
rag74 <rguarch@hotmail.com>
2024-06-08 09:56:33 -0300

    actualizacion somos 2024

--- src/components/Somos/Somos.jsx
+++ src/components/Somos/Somos.jsx
@@ -14,40 +14,51 @@ function Somos() {
         </div>
 
         <div className='somosTexto'>
-            <p>Impulsamos este espacio de expresión y encuentro colectivo para que todos y todas lean, escuchen y conozcan las miradas, las creaciones y la pluralidad de reflexiones que atraviesan a los y las adolescentes en nuestro tiempo.</p>
+            <p>Somos un grupo de estudiantes y profesores que conformamos este espacio de expresión y encuentro colectivo, en el que buscamos conformar una voz común a través del descubrimiento y la experiencia individual. Nuestro objetivo es que todos y todas puedan leer, escuchar y conocer los intereses y las ideas que atraviesan a los y las adolescentes, y lograr, de este modo, construir nuestra propia identidad.</p>
 
-            <p>A diario escuchamos hasta el hartazgo que "la juventud está perdida", “no sirve para nada” y está “metida en sus pantallas". Ahora bien, ¿qué pasa si usamos esas pantallas para visibilizar la potencia de la imaginación y el pensamiento, pero no a través de un filtro, sino mostrando nuestras producciones, sueños y compromiso para con el mundo en el que vivimos?</p>
+            <p>Como estudiantes de un colegio secundario ubicado en el sur de la Ciudad de Buenos Aires, en el barrio de Mataderos, nos comprometemos a defender nuestras voces, muchas veces subestimadas, entendiendo la importancia de expresarnos libremente.</p>
 
-            <p>Quizás piensen que nos falta experiencia, y quizás (también) tengan algo de razón, pero no por eso vamos a parar este viaje y eso es lo que nos hace construir, entre todos y todas, esta revista digital.</p>
+            <p>En este camino, nos valemos de las pantallas (en las que tanto tiempo pasamos) para visibilizar la potencia y el pensamiento presentes en nuestras producciones. Queremos reflexionar, escribir y crecer como personas.</p>
 
-            <p>Desde la escuela, un grupo de profes y estudiantes creemos que vale la pena pensar,  observar, imaginar y reflexionar acerca de lo que nos rodea. En este espacio, queremos invitarlos e invitarlas a compartir nuestro mundo, nuestras ideas y nuestras inquietudes; a leer y ser leídos, a escuchar y ser escuchados.</p>
+            <p>Los y las invitamos a compartir nuestro mundo, nuestras ideas y nuestras inquietudes: a leer y a ser leídos, a escuchar y ser escuchados.</p>
 
             <p>Subite a este colectivo de voces. La palabra ya está en marcha.</p>
         </div>
 
         <div className='somosCierre'>
             <img src="/img/altobondi-VByN.png" alt="logo altobondi" id="logovbyn" />
-            <p>Revista cultural del Instituto Plácido Marín</p>
+            <p>Subite a este colectivo de voces.</p>
+            <h3>La palabra ya está en marcha.</h3>
         </div>
     </section>
     <hr className='divisor'/>
     <section className='creditos'>
-        <h3>Equipo de trabajo ALTOBONDI 2023:</h3>
+        <h3>Equipo de trabajo ALTOBONDI 2024</h3>
         
-        <span>Coordinadoras generales:</span>
-        <p>Wayra Valdez y Sara Plenovich</p>
+        <span>Coordinadoras generales</span>
+        <p>Malena Cabrera y Tiziana Esquivel</p>
 
-        <span>Responsables de edición:</span>
-        <p>Malena Cabrera y Yolanda Próspero </p>
-
-        <span>Equipo de trabajo de diseño y redes sociales:</span>
-        <p>Sara Plenovich</p>
+        <span>Diseño y redes sociales</span>
         <p>Abril Angeleri</p>
-        <p>Agustina Alegre</p>
-        <p>Candela Caponeto</p>
-        <p>Uma Avila</p>
+        <p>Micaela Ibarra</p>
+        <p>Julieta Fernández</p>
+        <p>Mateo Imberlina</p>
+        <p>Valentina Guzman</p>
 
-        <span>Equipo de redacción:</span>
+        <span>Responsables de sección y escritores</span>
+        <p>Sara Plencovich (Ficción)</p>
+        <p>Giuliano Giufridda (Poesía)</p>
+        <p>Nahiara Rojo (Alto viaje)</p>
+        <p>Bárbara Robledo (Periodismo)</p>
+        <p>Matías Valiero (Miradas)</p>
+        <p>Tobías Cabrera</p>
+        <p>Lucas Cabrera</p>
+        <p>Avril Araujo</p>
+        <p>María Luz Flores</p>
+        <p>Sofía Petenello</p>
+        <p>Priscila Brégoli</p>
+
+        <span>Equipo de redacción</span>
         <p>Matias Valiero</p>
         <p>Tiziana Esquivel</p>
         <p>Lucio Ambrossio</p>
@@ -58,14 +69,15 @@ function Somos() {
         <p>Morena Avedikián</p>
 
         
-        <span>Coordinadores docentes:</span>
-        <p>Nora Sarda y Sebastián Gabriel Di Giorgio</p>
+        <span>Coordinadores docentes</span>
+        <p>Sebastián Gabriel Di Giorgio y Marianela Landini</p>
          
-        <span>Colaboradores docentes:</span>
-        <p>Juan Aladro</p>
+        <span>Colaboradores docentes</span>
+        <p>Juan Aladro y Paula Fernándezo</p>
 
-        <span>Diseño de revista y programación web:</span>
-        <img className='logorag' src="/img/poligonorag.png" alt="logo RAG" />
+        <br/>
+        <h3>Revista cultural del Instituto Plácido Marín</h3>
+        <img src="/img/logoplacidochico.png" alt="" id="logoplacido"/>
 
 
 
--- src/data/permisos.js
+++ src/data/permisos.js
@@ -1,1 +1,9 @@
-export const permisos = ['l7bGGeQjnJNqoaxhQ5cd9cJRAfW2', 'fEqyUn1whcPVJpUrEUQ9VQOWkep1', 'c8CYsgWnFXbb0SkHTUTgmTfNn8C3', 'JDhWhvRWhvSY86gUWR5IJDrNZWy2']
+export const permisos = [   'GzX7WiiQKccRYawPF1PKb1XKMlz2', 
+                            'CFaA4KmKXGVdXSV1wXio1EYC2983', 
+                            'mN2cPKg83lV9sVQvGW2TNMH22JJ2', 
+                            'T1JJ1P5fyFQnluSnQ6JIUalGmYG2',
+                            'd9DvxrHvf0UNl89pwu851bwH2B33',
+                            'oT2XbpEwyyaSOEZloEKrMv5J2jn2',
+                            'fEqyUn1whcPVJpUrEUQ9VQOWkep1',
+                            'l7bGGeQjnJNqoaxhQ5cd9cJRAfW2',
+                        ]
